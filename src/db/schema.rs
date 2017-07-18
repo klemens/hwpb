e492c2aed04120555fd1cb1538da28cc14e5c0f8
Klemens Schölhorn <klemens@schoelhorn.eu>
2017-07-18 13:51:05 +0200

    Add static database table definitions
    This allows compiling the project without a running postgres database.

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,4 +1,82 @@
-infer_schema!("dotenv:DATABASE_URL");
+// This can be used during development for generating the table
+// definitions from the database schema
+//infer_schema!("dotenv:DATABASE_URL");
+
+// These table definitions can be generated using `diesel print-schema`
+table! {
+    completions (group_id,
+    task_id) {
+        group_id -> Int4,
+        task_id -> Int4,
+        tutor -> Nullable<Text>,
+        completed_at -> Nullable<Timestamptz>,
+    }
+}
+
+table! {
+    days (id) {
+        id -> Text,
+    }
+}
+
+table! {
+    elaborations (group_id,
+    experiment_id) {
+        group_id -> Int4,
+        experiment_id -> Text,
+        rework_required -> Bool,
+        accepted -> Bool,
+        accepted_by -> Nullable<Text>,
+    }
+}
+
+table! {
+    events (id) {
+        id -> Int4,
+        day_id -> Text,
+        experiment_id -> Text,
+        date -> Date,
+    }
+}
+
+table! {
+    experiments (id) {
+        id -> Text,
+    }
+}
+
+table! {
+    group_mappings (student_id,
+    group_id) {
+        student_id -> Text,
+        group_id -> Int4,
+    }
+}
+
+table! {
+    groups (id) {
+        id -> Int4,
+        desk -> Int4,
+        day_id -> Text,
+        comment -> Text,
+    }
+}
+
+table! {
+    students (id) {
+        id -> Text,
+        name -> Text,
+    }
+}
+
+table! {
+    tasks (id) {
+        id -> Int4,
+        experiment_id -> Text,
+        name -> Text,
+    }
+}
+
 
 // This is a hack until arbitrary joins or "through associations" are allowed.
 // Also see https://github.com/diesel-rs/diesel/issues/938
