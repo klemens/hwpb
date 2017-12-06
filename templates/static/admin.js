96537b833a59b521271bc3e01b07369c2e558ae1
Klemens Schölhorn <klemens@schoelhorn.eu>
2017-12-06 23:12:58 +0100

    Allow creating new years through the admin page

--- templates/static/admin.js
+++ templates/static/admin.js
@@ -1,7 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let yearSelect = document.querySelector("header select");
-    yearSelect.addEventListener("change", () => {
-        let site = document.body.dataset.site;
-        document.location = "/admin/" + yearSelect.value + "/" + site;
-    });
+    document.querySelector("header select")
+        .addEventListener("change", onYearChange);
 });
+
+async function onYearChange(event) {
+    let target = event.target;
+
+    if(target.value === "new-year") {
+        let year = parseInt(prompt("Neues Jahr:"));
+        if(year === null) {
+            return;
+        }
+
+        try {
+            let url = "/api/year/" + year;
+
+            let response = await myfetch(url, {
+                method: "PUT"
+            });
+            if(!response.ok) {
+                throw "API error";
+            }
+
+            location = "/admin/" + year;
+        } catch(e) {
+            toast("error", e);
+        }
+    } else {
+        let site = document.body.dataset.site;
+        location = "/admin/" + target.value + "/" + site;
+    }
+}
